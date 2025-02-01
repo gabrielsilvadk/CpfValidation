da586ac0425d0c7e4165a925b911ab219f16248e
gabrielsilvadk <gabriel.silvadk@gmail.com>
2025-02-01 13:05:34 -0300

    Initial commit with the entiry project

---
+++ fnvalidacpf.cs
@@ -0,0 +1,103 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace httpValidaCpf
+{
+    public static class fnvalidacpf
+    {
+        [FunctionName("fnvalidacpf")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Iniciando a validação do CPF.");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            if (data == null)
+            {
+                return new BadRequestObjectResult("Por favor, informe o CPF.");
+            }
+            string cpf = data?.cpf;
+
+            if (ValidaCPF(cpf) == false)
+            {
+                return new BadRequestObjectResult("CPF inválido.");
+            }
+
+            var responseMessage = "CPF válido e limpo junto a Receita Federal.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+
+
+
+        public static bool ValidaCPF(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+                return false;
+
+            // Remove non-numeric characters
+            cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+            if (cpf.Length != 11)
+                return false;
+
+            // Check for invalid CPFs
+            string[] invalidCpfs = new string[]
+            {
+                "00000000000", "11111111111", "22222222222", "33333333333",
+                "44444444444", "55555555555", "66666666666", "77777777777",
+                "88888888888", "99999999999"
+            };
+
+            if (Array.Exists(invalidCpfs, element => element == cpf))
+                return false;
+
+            // Validate CPF digits
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            string digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+    }
+
+}
+
